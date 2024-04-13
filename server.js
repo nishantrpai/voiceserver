045b3c376277eab5cef7cbd905f0747c470ae3e9
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-04-13 21:21:41 +0530

    update console to show room id username

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ io.on("connection", (socket) => {
 
   socket.on("audio", ({roomId, username, audio}) => {
     // emit to all clients in the room
+    console.log(roomId, username, audio.length);
     console.log('send audio', audio.length);
     io.to(roomId).emit("audio", {username, audio});
   });
